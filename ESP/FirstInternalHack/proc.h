1c39eb1a0f67ec660cd478a52be218c1f8739e11
pphaoniubi <85983149+pphaoniubi@users.noreply.github.com>
2022-05-18 06:35:15 +0800

    Delete ESP directory

--- ESP/FirstInternalHack/proc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include <windows.h>
-#include <TlHelp32.h>
-//https://guidedhacking.com
-
-DWORD GetProcId(const wchar_t* procName);
-
-uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
