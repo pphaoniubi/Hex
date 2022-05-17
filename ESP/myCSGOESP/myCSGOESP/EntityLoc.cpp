c65439a3b4433157b09adacea230bf19b0bc920c
pphaoniubi <85983149+pphaoniubi@users.noreply.github.com>
2022-05-18 06:30:47 +0800

    Delete ESP/myCSGOESP directory

--- ESP/myCSGOESP/myCSGOESP/EntityLoc.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "EntityLoc.h"
-
-Vector3 GetEntityLoc(HANDLE handle, uintptr_t entListAddr, uintptr_t entityDist) {
-
-
-
-	
-}
--- ESP/myCSGOESP/myCSGOESP/hook.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void patch(BYTE* dst, BYTE* src, unsigned int size);
-
-bool Hook(char* src, char* dst, int len);
-
-char* trampHook(char* src, char* dst, unsigned int len);
